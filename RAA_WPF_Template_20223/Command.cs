bc81ea0fbbdc68eca048d34b25c8175c11158547
kilkellym <michael@archsmarter.com>
2023-03-22 16:11:05 -0400

    Add project files.

---
+++ RAA_WPF_Template_20223/Command.cs
@@ -0,0 +1,50 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+
+#endregion
+
+namespace RAA_WPF_Template_20223
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            // put any code needed for the form here
+            DockablePane myPane = commandData.Application.GetDockablePane(Globals.myPaneId);
+           
+            if(myPane.IsShown() == true)
+            {
+                myPane.Hide();
+            }
+            else
+                myPane.Show();
+
+            // get form data and do something
+
+            return Result.Succeeded;
+        }
+
+        public static String GetMethod()
+        {
+            var method = MethodBase.GetCurrentMethod().DeclaringType?.FullName;
+            return method;
+        }
+    }
+}
---
+++ RAA_WPF_Template_20223/Globals.cs
@@ -0,0 +1,15 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAA_WPF_Template_20223
+{
+    internal static class Globals
+    {
+        public static Guid myGUID;
+        public static DockablePaneId myPaneId;
+    }
+}
